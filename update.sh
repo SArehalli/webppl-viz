45ee7badfe822bb91c1a2c74856477928cdcbdb7
Long Ouyang <longouyang@gmail.com>
2016-03-10 12:59:06 -0800

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git checkout master
+make demo/webppl-viz.js
+cp demo/webppl-viz.js webppl-viz-new.js
+git checkout gh-pages
+mv webppl-viz.js webppl-viz-old.js
+mv webppl-viz-new.js webppl-viz.js
