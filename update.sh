253bc3ec95cabb4f332193fc5da1bfe668c1f982
Long Ouyang <longouyang@gmail.com>
2016-03-23 20:35:02 -0700

    Abandon hardlinks to webppl-editor
    They don't work like I thought they would -- updating the original
    doesn't seem to propogate to the hardlinked version

--- update.sh
+++ update.sh
@@ -6,3 +6,5 @@ cp demo/webppl-viz.js webppl-viz-new.js
 git checkout gh-pages
 mv webppl-viz.js webppl-viz-old.js
 mv webppl-viz-new.js webppl-viz.js
+cp ../wp-editor/compiled/editor.js .
+cp ../wp-editor/compiled/editor.css .
