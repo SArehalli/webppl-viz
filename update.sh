9f869e1779b6fa0e1d47e671a7ae4c1e58f1d495
Long Ouyang <longouyang@gmail.com>
2016-03-25 14:42:06 -0700

    Change update.sh (assume demo and src repos are parallel dirs)

--- update.sh
+++ update.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-git checkout master
-make demo/webppl-viz.js
-cp demo/webppl-viz.js webppl-viz-new.js
-git checkout gh-pages
-mv webppl-viz.js webppl-viz-old.js
-mv webppl-viz-new.js webppl-viz.js
-cp ../wp-editor/compiled/editor.js .
-cp ../wp-editor/compiled/editor.css .
+rsync -rL ../wviz/demo/ .
